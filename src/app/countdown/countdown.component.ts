bbea641b884ce0e440200febc5c1db82396ad497
Amit Dubey <akd6882@gmail.com>
2021-04-06 16:40:06 +0530

    progressbar running with counter

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
   templateUrl: './countdown.component.html',
   styleUrls: ['./countdown.component.scss']
 })
-export class CountdownComponent{
+export class CountdownComponent implements OnInit{
+
+  @Input() init: number = null;
+  public counter: number = 0;
+
+  @Output() onDecrese = new EventEmitter<number>();
+  @Output() onComplete = new EventEmitter<void>();
 
   constructor() { }
+  
+  ngOnInit(): void {
+    this.startCountdown();
+  }
+
+  startCountdown() {
+    if(this.init && this.init > 0) {
+      this.counter = this.init;
+      this.doCountdown();
+    }
+  }
+
+  doCountdown() {
+    setTimeout(() => {
+      this.counter = this.counter - 1;
+      this.processCountdown();
+    }, 1000);
+  }
+
+  processCountdown() {
+    this.onDecrese.emit(this.counter);
+    console.log('count is: ', this.counter);
+
+    if(this.counter === 0) {
+      this.onComplete.emit();
+      console.log('--counter end--');
+    } else {
+      this.doCountdown();
+    }
+  }
 
 }
