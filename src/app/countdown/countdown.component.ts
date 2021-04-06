a10ace5d5742b20b2c7e7e8d4d18201e3db9dfcc
Amit Dubey <akd6882@gmail.com>
2021-04-06 17:23:40 +0530

    countdownTimerRef code update

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -42,8 +42,10 @@ export class CountdownComponent implements OnInit, OnChanges, OnDestroy{
   }
   
   private clearTimeout() {
-    clearTimeout(this.countdownTimerRef);
-    this.countdownTimerRef = null;
+    if(this.countdownTimerRef) {
+      clearTimeout(this.countdownTimerRef);
+      this.countdownTimerRef = null;
+    }
   }
   
   processCountdown() {
